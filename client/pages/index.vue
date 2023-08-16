29734197d3c790abd29f423b39a06202f8ad8d20
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-16 16:56:40 -0400

    added line spacing option

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <b-row class="gap-1 flex-grow-1">
-    <b-col class="pr-2 h-100" :class="printable ? '' : 'overflow-auto'">
+    <b-col class="pr-2 h-100" :class="printable ? '' : 'overflow-auto'" :style="`line-height: ${lineSpacing}rem;`">
       <span class="pre-wrap" :style="`font-size: ${fontSize}pt;`">{{ text }}</span>
     </b-col>
     <b-col class="d-flex flex-column h-100" :style="downloading ? 'height: 765px !important;' : ''">
       <b-row class="flex-grow-1 gap-1 mr-1">
         <!-- <b-col v-for="(col, i) in observations" class="px-0 d-flex flex-column gap-1" :key="`col-${i}`"> -->
-          <template v-for="(v, k) in observations">
-            <b-card v-if="v" :key="k" class="flex-grow-1 observation" body-class="px-2 py-1">
-              {{ k }}
-            </b-card>
-          </template>
+        <template v-for="(v, k) in observations">
+          <b-card v-if="v" :key="k" class="flex-grow-1 observation" body-class="px-2 py-1">
+            {{ k }}
+          </b-card>
+        </template>
 
         <!-- </b-col> -->
       </b-row>
@@ -29,7 +29,7 @@ export default {
     }
   },
   computed: {
-    ...mapState(["textOptions", "observations", "passages", "fontSize", "printable", "downloading"]),
+    ...mapState(["textOptions", "observations", "passages", "fontSize", "printable", "downloading", "lineSpacing"]),
     text() {
       return this.passages.join("\n\n")
         .replace(/\n +/gm, "\n")
