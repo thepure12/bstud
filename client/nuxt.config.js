235c5bb05fc63b45aa4b27f56394a09a76c52d12
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-18 16:14:32 -0400

    changed name

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client-bootstrap',
+    title: 'Vathéos',
     htmlAttrs: {
       lang: 'en'
     },
