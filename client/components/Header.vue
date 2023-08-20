bbc2ee5c8d4d07009d083d2c9b70e2989b63cd05
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-08-19 20:58:20 -0400

    updaed favicon and menu button

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,15 +1,24 @@
 <template>
     <b-navbar class="flex-wrap justify-content-start" type="dark" variant="primary" toggleable sticky>
-        <b-navbar-toggle target="menu" class="border-white"></b-navbar-toggle>
-        <b-navbar-brand>
+        <!-- <b-navbar-toggle target="menu" class="bg-secondary text-white py-2">
+            <template #default="{ expanded }">
+                <b-icon icon="gear"></b-icon>
+                Vathéos
+            </template>
+        </b-navbar-toggle> -->
+        <!-- <b-navbar-brand>
             Vathéos
-        </b-navbar-brand>
+        </b-navbar-brand> -->
         <b-form class="flex-grow-1" @submit.prevent="onSearch">
-            <b-input-group class="search-group">
+            <b-input-group>
                 <b-form-input v-model="search" type="search" placeholder="Search"></b-form-input>
-                <!-- <template #prepend>
-                    <b-btn class="print" @click="$store.commit('setPrintable', true)">Print</b-btn>
-                </template> -->
+                <template #prepend>
+                    <!-- <b-btn class="print" @click="$store.commit('setPrintable', true)">Print</b-btn> -->
+                    <b-btn v-b-toggle:menu>
+                        <b-icon icon="gear"></b-icon>
+                        Vathéos
+                    </b-btn>
+                </template>
                 <template #append>
                     <b-btn type="submit">Search</b-btn>
                 </template>
@@ -19,7 +28,11 @@
     </b-navbar>
 </template>
 <script>
+import { BIcon, BIconThreeDotsVertical, BIconGear } from "bootstrap-vue"
 export default {
+    components: {
+        BIcon, BIconThreeDotsVertical, BIconGear
+    },
     data() {
         return {
             search: ""
