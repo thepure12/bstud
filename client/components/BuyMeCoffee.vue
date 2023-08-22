5f602395f15ad5b88d8fe32bcb56b124947ebe03
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-22 08:27:22 -0400

    added buy me coffee component

---
+++ client/components/BuyMeCoffee.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bmc-btn-container">
+        <b-btn href="https://buymeacoffee.com/thepure12" target="_blank">
+            <span class="bmc-btn-text">☕ Buy me a coffee</span>
+        </b-btn>
+    </div>
+</template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Cookie&amp;display=swap");
+
+.bmc-btn-container {
+    margin-inline: auto;
+}
+
+.bmc-btn-text {
+    display: inline-block;
+    line-height: 0;
+    width: 100%;
+    flex-shrink: 0;
+    font-family: [FONT] !important;
+}
+
+.logo-outline {
+    fill: #ffffff;
+}
+
+.logo-coffee {
+    fill: #FFDD00;
+}
+</style>
