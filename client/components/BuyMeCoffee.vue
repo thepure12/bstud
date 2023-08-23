8487463b8de8e3e6be2872af3e75fdbb82da1bb5
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-08-23 11:44:14 -0400

    Added translation selection

--- client/components/BuyMeCoffee.vue
+++ client/components/BuyMeCoffee.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-btn href="https://buymeacoffee.com/thepure12" target="_blank">
+        <b-btn variant="primary" href="https://buymeacoffee.com/thepure12" target="_blank">
             <span class="bmc-btn-text">☕ Buy me a coffee</span>
         </b-btn>
     </div>
