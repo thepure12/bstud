8f110b7f39f6497141bd27ee22bae5d1bce065c6
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-08-23 10:52:31 -0400

    added other translations to api

--- server/resources/text.py
+++ server/resources/text.py
@@ -6,6 +6,7 @@ import os
 from string import Template
 import re
 import toml
+import gateway_scrape
 
 try:
     config = toml.load("config.toml")
@@ -36,27 +37,35 @@ class PassageText(Resource):
         pass
 
     def get(self):
-        res = requests.get(ESV_URL, headers=HEADERS, params=self.params)
-        if "download" in self.params:
-            html_path = os.path.abspath("worksheet.html")
-            css_path = os.path.abspath("worksheet.css")
-            with open(html_path) as tpl_file:
-                tpl = Template(tpl_file.read())
-            with open(css_path) as css_file:
-                css = css_file.read()
-            text = self._formatText(res.json()["passages"])
-            worksheet_text = tpl.substitute(
-                text=text, style=css, font_size=self.params.get("font-size") or 10
-            )
-            # print("Converting")
-            converter.convert(
-                f"data:text/html;charset=utf-8,{worksheet_text}",
-                "out.pdf",
-                print_options=PRINT_OPTIONS,
-            )
-            # print("Converted")
-            filename = (
-                f"{self.params.q}.pdf" if self.params.q else "bible_study_worksheet.pdf"
-            )
-            return static_file("out.pdf", "./", download=filename)
-        return res.json()
+        version = self.params.get("version", "ESV")
+
+        if version=="ESV":
+            res = requests.get(ESV_URL, headers=HEADERS, params=self.params)
+            return res.json()
+        else:
+            return gateway_scrape.getText(search=self.params.q, version=version)
+            
+
+        # if "download" in self.params:
+        #     html_path = os.path.abspath("worksheet.html")
+        #     css_path = os.path.abspath("worksheet.css")
+        #     with open(html_path) as tpl_file:
+        #         tpl = Template(tpl_file.read())
+        #     with open(css_path) as css_file:
+        #         css = css_file.read()
+        #     text = self._formatText(res.json()["passages"])
+        #     worksheet_text = tpl.substitute(
+        #         text=text, style=css, font_size=self.params.get("font-size") or 10
+        #     )
+        #     # print("Converting")
+        #     converter.convert(
+        #         f"data:text/html;charset=utf-8,{worksheet_text}",
+        #         "out.pdf",
+        #         print_options=PRINT_OPTIONS,
+        #     )
+        #     # print("Converted")
+        #     filename = (
+        #         f"{self.params.q}.pdf" if self.params.q else "bible_study_worksheet.pdf"
+        #     )
+        #     return static_file("out.pdf", "./", download=filename)
+            
