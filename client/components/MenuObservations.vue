122c161b6525214642988cfb9f0d1df426f4b5dc
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-23 15:29:30 -0400

    moved sidebar items to components

---
+++ client/components/MenuObservations.vue
@@ -0,0 +1,39 @@
+<template>
+    <b-card bg-variant="dark" header-class="bg-secondary h5">
+        <template #header>
+            Observations
+            <b-link href="https://www.knowableword.com/2013/04/12/how-to-study-the-bible/#:~:text=Bible-,Observation"
+                target="_blank">
+                <b-icon icon="info-circle" class="ml-2" variant="white" scale=1></b-icon>
+            </b-link>
+        </template>
+        <b-form>
+            <template v-for="(v, k) in observations">
+                <b-form-checkbox :checked="v" @input="v => setObservation({ observation: k, value: v })" size="lg">
+                    {{ k }}
+                </b-form-checkbox>
+
+            </template>
+        </b-form>
+    </b-card>
+</template>
+<script>
+import { mapState, mapMutations } from 'vuex'
+import { BIcon, BIconInfoCircle } from 'bootstrap-vue'
+export default {
+    components: {
+        BIcon,
+        BIconInfoCircle
+    },
+    computed: {
+        ...mapState([
+            "observations"
+        ])
+    },
+    methods: {
+        ...mapMutations([
+            "setObservation"
+        ]),
+    }
+}
+</script>
