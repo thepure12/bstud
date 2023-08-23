966f222e53976b4995e67dc3f0ebcd10151c7a56
TJ Renninger <renningert@LAPTOP-LLEJQRTO>
2023-08-23 00:36:10 -0400

    added feedback page

---
+++ client/pages/feedback.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfGNqwLPqNV94A1NfHEhlwiRZxKCQTBJlolIpl4n_iiAXR58Q/viewform?embedded=true"
+            width="100%" height="1000px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+</template>
