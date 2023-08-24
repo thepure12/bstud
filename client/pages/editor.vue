73d8e44116869ec06ad7bfb7219a20abe7256286
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-24 12:05:51 -0400

    moved sheets to component and added it to editor

--- client/pages/editor.vue
+++ client/pages/editor.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="m-3 ">
-        <b-row v-if="canvas" class="mb-3 justify-content-around">
+        <b-row v-if="canvas" class="mb-3 justify-content-around border-bottom">
             <b-form-group label="Color">
                 <verte picker="square" :value="canvas.freeDrawingBrush.color" @input="onColorChange"></verte>
             </b-form-group>
@@ -9,14 +9,27 @@
                     max="50" step="1"></b-form-input>
             </b-form-group>
             <b-form-group class="my-auto">
-                <b-btn @click="onUndo">Undo</b-btn>
+                <b-row class="gap-1">
+                    <b-btn @click="onUndo">
+                        <b-icon icon="arrow90deg-left"></b-icon>
+                    </b-btn>
+                    <b-btn @click="onRedo">
+                        <b-icon icon="arrow90deg-right"></b-icon>
+                    </b-btn>
+                    <b-btn id="trash">
+                        <b-icon icon="trash"></b-icon>
+                    </b-btn>
+                    <b-popover target="trash" triggers="click blur" placement="bottom">
+                        <b-btn @click="onTrash">Erase All</b-btn>
+                    </b-popover>
+                </b-row>
             </b-form-group>
         </b-row>
         <div id="canvas-container" class="mx-auto">
-            <div class="m-3 position-absolute">
-                {{ $store.state.passages[0] }}
+            <div class="position-absolute">
+                <Sheets :textContainers.sync="textContainers"></Sheets>
             </div>
-            <canvas class="border" id="canvas" ref="canvas">
+            <canvas class="mt-3" id="canvas" ref="canvas">
             </canvas>
         </div>
 
@@ -25,7 +38,7 @@
 <script>
 import Verte from 'verte';
 import 'verte/dist/verte.css';
-
+import { BIcon, BIconArrow90degLeft, BIconArrow90degRight, BIconTrash } from 'bootstrap-vue'
 export default {
     head() {
         return {
@@ -41,17 +54,21 @@ export default {
         }
     },
     components: {
-        Verte
+        Verte,
+        BIcon,
+        BIconArrow90degLeft,
+        BIconArrow90degRight,
+        BIconTrash
     },
     data() {
         return {
+            textContainers: [],
             canvas: null,
             brush: null,
             brushColor: "black",
             brushThickness: 1,
-            suckerCanvas: null,
-            suckerArea: [],
-            isSucking: false
+            h: [],
+            isRedoing: false,
         }
     },
     computed: {
@@ -66,10 +83,14 @@ export default {
             const container = document.querySelector("#canvas-container")
             this.canvas.setDimensions({ width: container.clientWidth, height: container.clientHeight })
             document.querySelector(".canvas-container").style.position = "absolute !important"
+            this.canvas.on('object:added', function () {
+                if (!this.isRedoing) {
+                    this.h = [];
+                }
+                this.isRedoing = false;
+            });
         },
         onColorChange(color) {
-            // const { r, g, b, a } = color.rgba
-            // this.canvas.freeDrawingBrush.color = `rgba(${r}, ${g}, ${b}, ${a})`
             this.canvas.freeDrawingBrush.color = color
             this.brushColor = color
         },
@@ -80,9 +101,20 @@ export default {
         },
         onUndo() {
             if (this.canvas._objects.length > 0) {
-                this.canvas._objects.pop()
+                this.h.push(this.canvas._objects.pop())
                 this.canvas.renderAll()
             }
+        },
+        onRedo() {
+            if (this.h.length > 0) {
+                this.isRedoing = true;
+                this.canvas.add(this.h.pop());
+            }
+        },
+        onTrash() {
+            this.$root.$emit('bv::hide::popover', 'trash')
+            this.canvas._objects.forEach(o => this.h.push(o))
+            this.canvas.clear()
         }
     },
     mounted() {
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,24 +1,10 @@
 <template>
   <div>
-    <!-- <a></a> -->
-    <b-col id="sheets" class="d-flex flex-column gap-1">
-      <template v-for="(text, k) in sheetTexts">
-        <b-row v-if="isSheetVisible(text, k)" :id="k" class="flex-grow-1 gap-1 overflow-auto" :key="k"
-          :ref="`textContainer`" :class="printable ? 'printable-sheet' : 'sheet'">
-          <b-col v-if="text || k === 'sheet1'" class="overflow-auto mr-2">
-            <span class="pre-wrap" :style="`font-size: ${fontSize}pt; line-height: ${lineSpacing}rem;`">{{ text }}</span>
-          </b-col>
-          <!-- Observations -->
-          <Observations v-if="k === 'sheet1'"></Observations>
-          <!-- Questions -->
-          <Questions v-if="k === 'sheet2' && totalQuestions"></Questions>
-          <!-- Applications -->
-          <Applications v-if="k === 'sheet2' && !text && totalApplications"></Applications>
-          <Applications v-if="k === 'sheet3' && totalApplications"></Applications>
-        </b-row>
-      </template>
-    </b-col>
-    <div v-if="text && !printable" class="fixed-bottom d-flex justify-content-end gap-1 p-2 pr-4">
+    <Sheets :textContainers.sync="textContainers"></Sheets>
+    <div v-if="passages && !printable" class="fixed-bottom d-flex justify-content-end gap-1 p-2 pr-4">
+      <b-btn to="editor">
+        <b-icon icon="pencil"></b-icon>
+      </b-btn>
       <b-btn class="btn-rotate" @click="rotateDevice">
         <b-iconstack>
           <b-icon stacked icon="phone" scale="0.6"></b-icon>
@@ -34,7 +20,7 @@
 </template>
 <script>
 import { mapState, mapMutations } from "vuex"
-import { BIcon, BIconDownload, BIconPhone, BIconstack, BIconArrowClockwise } from 'bootstrap-vue'
+import { BIcon, BIconDownload, BIconPhone, BIconstack, BIconArrowClockwise, BIconPencil } from 'bootstrap-vue'
 
 export default {
   components: {
@@ -42,134 +28,27 @@ export default {
     BIconstack,
     BIconDownload,
     BIconPhone,
-    BIconArrowClockwise
+    BIconArrowClockwise,
+    BIconPencil
   },
   data() {
     return {
-      sheetTexts: {
-        sheet1: "",
-        sheet2: "",
-        sheet3: "",
-        sheet4: "",
-        sheet5: "",
-        sheet6: "",
-        sheet7: "",
-        sheet8: "",
-        sheet9: "",
-        sheet10: "",
-      },
-      textContainers: [],
-      numOfSheets: 1
+      textContainers: []
     }
   },
   computed: {
     ...mapState([
       "textOptions",
-      "observations",
-      "questions",
-      "applications",
       "passages",
-      "fontSize",
       "printable",
-      "downloading",
-      "lineSpacing",
-      "trimming"
+
     ]),
-    text() {
-      let text = this.passages.join("\n\n")
-        .replace(/\n +/gm, "\n")
-        .replace(/ {2,}/gm, " ")
-        .replace(/\n{2,}/gm, "\n")
-        .trim()
-      return text
-    },
-    totalQuestions() {
-      return Object.values(this.questions).reduce((a, b) => a + b, 0)
-    },
-    totalApplications() {
-      return Object.values(this.applications).reduce((a, b) => a + b, 0)
-    },
     downloadUrl() {
       return `http://localhost:8000/text?${new URLSearchParams(this.textOptions)}&font-size=${this.fontSize}&download`
     }
   },
-  watch: {
-    text() {
-      this.trimText(1)
-    },
-    fontSize() {
-      this.trimText(1)
-    },
-    lineSpacing() {
-      this.trimText(1)
-    },
-    totalQuestions(oldVal, newVal) {
-      if (oldVal === 0 || newVal == 0)
-        this.trimText(1)
-    },
-    totalApplications(oldVal, newVal) {
-      if (oldVal === 0 || newVal == 0)
-        this.trimText(1)
-    }
-  },
   methods: {
-    ...mapMutations(["setDownloading", "setPrintable", "setTrimming"]),
-    async trimText(sheet) {
-      if (sheet === 1 && this.trimming) return
-      this.setTrimming(true)
-      if (sheet === 1) {
-        this.resetSheetText()
-        this.sheetTexts[`sheet${sheet}`] = this.text
-      }
-      await this.$nextTick()
-      while (
-        this.textContainers[sheet - 1].scrollHeight - this.textContainers[sheet - 1].clientHeight > 0
-      ) {
-        let lastSentenctIndex = this.sheetTexts[`sheet${sheet}`].lastIndexOf(".", this.sheetTexts[`sheet${sheet}`].lastIndexOf(".") - 1) + 1;
-        if (lastSentenctIndex === 0) break
-        let lastSentence = this.sheetTexts[`sheet${sheet}`].substring(lastSentenctIndex).trim()
-        this.sheetTexts[`sheet${sheet + 1}`] = `${lastSentence} ${this.sheetTexts[`sheet${sheet + 1}`] || ''}`
-        this.sheetTexts[`sheet${sheet}`] = this.sheetTexts[`sheet${sheet}`].substring(0, lastSentenctIndex);
-        await this.$nextTick()
-      }
-      if (this.textContainers.length > sheet)
-        this.trimText(sheet + 1)
-      else
-        this.setTrimming(false)
-    },
-    resetSheetText() {
-      for (const [sheet, value] of Object.entries(this.sheetTexts)) {
-        this.sheetTexts[sheet] = ""
-      }
-    },
-    async scaleSheets() {
-      await this.$nextTick()
-      const scrollTainer = document.querySelector(".scrolltainer")
-      const root = document.querySelector(":root")
-      root.style.setProperty("--scale", 1)
-      let scale = getComputedStyle(root).getPropertyValue("--scale")
-      while (scrollTainer.scrollWidth > scrollTainer.clientWidth && scale > 0) {
-        root.style.setProperty("--scale", parseFloat(scale) - .01)
-        scale = getComputedStyle(root).getPropertyValue("--scale")
-        await this.$nextTick()
-      }
-    },
-    isSheetVisible(text, sheet) {
-      switch (sheet) {
-        case "sheet1":
-          return true
-        case "sheet2":
-          return text != "" || this.totalQuestions > 0 || this.totalApplications > 0
-        case "sheet3":
-          if (text) {
-            return true
-          } else {
-            return this.sheetTexts.sheet2 != "" && this.totalQuestions > 0 && this.totalApplications > 0
-          }
-        default:
-          return text != ""
-      }
-    },
+    ...mapMutations(["setDownloading", "setPrintable"]),
     rotateDevice() {
       if (screen.orientation.type === "landscape-primary") {
         document.exitFullscreen()
@@ -177,7 +56,6 @@ export default {
         document.exitFullscreen()
         document.body.requestFullscreen()
           .then(() => screen.orientation.lock("landscape-primary")
-            // .then(() => document.exitFullscreen())
             .catch())
       }
 
@@ -186,7 +64,6 @@ export default {
       this.setDownloading(true)
       this.setPrintable(true)
       this.$nextTick(() => {
-        // let element = document.querySelector("#sheets")
         let opt = {
           margin: .25,
           filename: this.textOptions.q,
@@ -211,22 +88,6 @@ export default {
         })
       })
     }
-  },
-  created() {
-    let portrait = window.matchMedia("(orientation: portrait)");
-    let scaleSheets = this.scaleSheets
-
-    portrait.addEventListener("change", function (e) {
-      if (e.matches) {
-        // Portrait mode
-      } else {
-        scaleSheets()
-      }
-    })
-  },
-  mounted() {
-    this.textContainers = this.$refs["textContainer"]
-    this.scaleSheets()
   }
 }
 </script>
