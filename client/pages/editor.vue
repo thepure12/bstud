8f09271399a783309ca37ab2f6e44ae35f8d76f8
thepure12 <5061163+thepure12@users.noreply.github.com>
2023-08-25 00:56:41 -0400

    moved canvas to sheets

--- client/pages/editor.vue
+++ client/pages/editor.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="m-3 ">
-        <b-row v-if="canvas" class="mb-3 justify-content-around"
-            :style="`border-bottom: ${brushThickness}px solid ${brushColor}`">
+    <div>
+        <b-row class="mb-3 justify-content-around" :style="`border-bottom: ${brushWidth}px solid ${brushColor}`">
             <b-form-group class="my-auto">
                 <b-btn to="/">
                     <b-icon icon="arrow-left"></b-icon>
@@ -29,8 +28,8 @@
                 </b-row>
             </b-form-group>
             <b-form-group label="Thickness">
-                <b-form-input type="range" :value="canvas.freeDrawingBrush.width" @input="onThicknessChange" min="1"
-                    max="50" step="1"></b-form-input>
+                <b-form-input type="range" :value="brushWidth" @input="onThicknessChange" min="1" max="50"
+                    step="1"></b-form-input>
             </b-form-group>
             <b-form-group class="my-auto">
                 <b-row class="gap-1">
@@ -56,19 +55,14 @@
                 </b-row>
             </b-form-group>
         </b-row>
-        <div id="canvas-container" class="mx-auto">
-            <div id="sheets-container" class="position-absolute">
-                <Sheets :textContainers.sync="textContainers" @trimmed="onTextTrimmed"></Sheets>
-            </div>
-            <canvas class="mt-3" id="canvas" ref="canvas">
-            </canvas>
-        </div>
-
+        <Sheets :textContainers.sync="textContainers" :drawing="true" :brushColor="brushColor" :brushWidth="brushWidth">
+        </Sheets>
     </div>
 </template>
 <script>
 import Verte from 'verte';
 import 'verte/dist/verte.css';
+import { mapState, mapMutations } from 'vuex'
 import {
     BIcon,
     BIconArrow90degLeft,
@@ -81,19 +75,6 @@ import {
     BIconEraser
 } from 'bootstrap-vue'
 export default {
-    head() {
-        return {
-            script: [
-                {
-                    hid: "fabric",
-                    src: 'https://unpkg.com/fabric@5.3.0/dist/fabric.min.js',
-                    callback: () => {
-                        this.setUpCanvas()
-                    }
-                }
-            ]
-        }
-    },
     components: {
         Verte,
         BIcon,
@@ -109,50 +90,26 @@ export default {
     data() {
         return {
             textContainers: [],
-            canvas: null,
-            brush: null,
             brushColor: "#000000",
             brushColorHistory: ["#4682b4", "#6a5acd", "#ff5733"],
-            brushThickness: 1,
+            brushWidth: 1,
             h: [],
             isRedoing: false,
             isFullscreen: false
         }
     },
     computed: {
-
     },
     methods: {
-        setUpCanvas() {
-            this.canvas = new fabric.Canvas("canvas", {
-                isDrawingMode: true
-            })
-            this.brush = this.canvas.freeDrawingBrush
-            const container = document.querySelector("#sheets-container")
-            this.canvas.setDimensions({ width: container.clientWidth, height: container.clientHeight })
-            document.querySelector(".canvas-container").style.position = "absolute !important"
-            this.canvas.on('object:added', function () {
-                if (!this.isRedoing) {
-                    this.h = [];
-                }
-                this.isRedoing = false;
-            });
-        },
-        onTextTrimmed() {
-            if (this.canvas) {
-                const container = document.querySelector("#sheets-container")
-                this.canvas.setDimensions({ width: container.clientWidth, height: container.clientHeight })
-                console.log("Text trimmed");
-            }
+        onSetBrush(brush) {
+
         },
         onColorChange(color) {
-            this.canvas.freeDrawingBrush.color = color
             this.brushColor = color
         },
         onThicknessChange(thickness) {
             thickness = parseInt(thickness)
-            this.canvas.freeDrawingBrush.width = thickness
-            this.brushThickness = thickness
+            this.brushWidth = thickness
         },
         onUndo() {
             if (this.canvas._objects.length > 0) {
@@ -183,20 +140,6 @@ export default {
         }
     },
     mounted() {
-
     }
 }
 </script>
-<style>
-#canvas-container {
-    position: relative;
-    /* width: 11in;
-    height: 8.5in; */
-}
-
-.canvas-container {
-    top: 0;
-    left: 0;
-    position: absolute !important;
-}
-</style>
